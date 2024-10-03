5f79f31bae28faab3bc0628e75ab0efaf3edb212
AlcatrazEscapee <alex@molleroneill.com>
2024-10-02 21:48:44 -0400

    Pin year-old VG version to avoid gradle updates and un-break workspace

--- Common/build.gradle.kts
+++ Common/build.gradle.kts
@@ -1,6 +1,13 @@
+// Later versions of vanillagradle are broken without newer versions of gradle
+// This is pinned to a version that works, since for the rest of 1.20.1 lifecycle, we're not expecting to update this, or gradle
+// In 1.21 this will move to MDG instead
+buildscript {
+    dependencies.add("classpath", "org.spongepowered:vanillagradle:0.2.1-20230929.005621-65")
+}
+
 plugins {
     java
-    id("org.spongepowered.gradle.vanilla") version "0.2.1-SNAPSHOT"
+    id("org.spongepowered.gradle.vanilla") version "0.2.1-20230929.005621-65"
 }
 
 // From gradle.properties
